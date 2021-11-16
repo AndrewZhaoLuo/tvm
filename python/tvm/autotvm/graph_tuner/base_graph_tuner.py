5d80ca4f86e30d16f5fb4fbc5996347784aefe74
Andrew Zhao Luo <andrew.zhao.luo@gmail.com>
2021-11-16 14:56:08 -0800

    revert changes to base_graph_tuner.py

--- python/tvm/autotvm/graph_tuner/base_graph_tuner.py
+++ python/tvm/autotvm/graph_tuner/base_graph_tuner.py
@@ -28,7 +28,6 @@ from tvm import autotvm, relay
 from tvm.autotvm.task import get_config
 from tvm.autotvm.record import encode, load_from_file
 from tvm.autotvm.measure import MeasureResult, MeasureInput
-from tvm.target import Target
 
 from ...target import Target
 from .utils import (
@@ -166,7 +165,7 @@ class BaseGraphTuner(object):
         if isinstance(graph, relay.function.Function):
             node_dict = {}
             graph = bind_inputs(graph, input_shapes, dtype)
-            expr2graph(graph, self._target_ops, node_dict, self._node_list, target)
+            expr2graph(graph, self._target_ops, node_dict, self._node_list)
         else:
             raise RuntimeError("Unsupported graph type: %s" % str(type(graph)))
 
@@ -218,14 +217,13 @@ class BaseGraphTuner(object):
     def _fetch_cfg(self):
         """Read and pre-process input schedules."""
         if isinstance(self._records, str):
-            # records = load_from_file(self._records)
-            records = tvm.auto_scheduler.measure_record.load_records(self._records)
+            records = load_from_file(self._records)
         else:
             records = self._records
         cfg_dict = {}
         for record in records:
             in_measure, _ = record
-            workload = in_measure.task.workload_key
+            workload = in_measure.task.workload
             if workload not in cfg_dict:
                 cfg_dict[workload] = []
             cfg_dict[workload].append(record)
@@ -242,7 +240,6 @@ class BaseGraphTuner(object):
             record_candidates = []
             infer_layout_func = get_infer_layout(node_entry["topi_op"][0])
             layout_tracking_dict = {}
-            breakpoint()
             for record in cfg_dict[workload]:
                 in_measure, out_measure = record
                 workload = in_measure.task.workload
@@ -370,14 +367,13 @@ class BaseGraphTuner(object):
         timeout=10,
         use_rpc=False,
         device_key=None,
-        host="127.0.0.1",
+        host="localhost",
         port=9190,
         n_parallel=1,
         build_func="default",
         layout_records=None,
         target_host=None,
         infer_layout=False,
-        runner=None,
     ):
         """Benchmark all possible layout transformation in the graph,
         given a set of schedule candidates for each workload of target operator.
@@ -441,12 +437,8 @@ class BaseGraphTuner(object):
             of benchmarking on target device.
 
             This might bring performance loss comparing to benchmarking layout transformation.
-        runner : Runner, optional
-            Accept a user-supplied runner
         """
         self._logger.info("Start to benchmark layout transformation...")
-        self._target, target_host = Target.check_and_update_host_consist(self._target, target_host)
-
         if layout_records is None and infer_layout:
             raise RuntimeError("Requires some records to infer layout transformation time.")
 
@@ -488,6 +480,7 @@ class BaseGraphTuner(object):
             return _callback
 
         builder = autotvm.LocalBuilder(n_parallel=n_parallel, build_func=build_func)
+        runner = autotvm.LocalRunner(number=min_exec_num, repeat=1, timeout=timeout)
         if use_rpc:
             if device_key is None:
                 raise RuntimeError("device_key need to be set to use rpc tracker mode.")
@@ -500,8 +493,6 @@ class BaseGraphTuner(object):
                 repeat=1,
                 timeout=timeout,
             )
-        elif not runner:
-            runner = autotvm.LocalRunner(number=min_exec_num, repeat=1, timeout=timeout)
         measure_option = autotvm.measure_option(builder=builder, runner=runner)
         for args in args_list:
             data, in_layout, out_layout = args
@@ -534,7 +525,9 @@ class BaseGraphTuner(object):
                 continue
 
             records = []
-            task = autotvm.task.create("layout_transform", args=args, target=self._target)
+            task = autotvm.task.create(
+                "layout_transform", args=args, target=self._target, target_host=target_host
+            )
             tuner = autotvm.tuner.GridSearchTuner(task)
             tuner.tune(n_trial=1, measure_option=measure_option, callbacks=[_log_to_list(records)])
             if not isinstance(records[0][1].costs[0], float):
